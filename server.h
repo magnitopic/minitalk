45e67e0ff415418562bd2a0f74d7147c3ecd75b8
magnitopic <magnitopic@gmail.com>
2022-11-24 18:07:13 +0100

    norm & gitignore

--- server.h
+++ server.h
@@ -6,16 +6,16 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 12:18:54 by alaparic          #+#    #+#             */
-/*   Updated: 2022/11/22 12:54:05 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/11/24 18:06:46 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SERVER_H
 # define SERVER_H
 
-#include<stdio.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include<signal.h>
+# include<stdio.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include<signal.h>
 
 #endif
