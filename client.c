c33dbcdeb9a8910d2937ed75b11ec983edcc3eb2
magnitopic <magnitopic@gmail.com>
2022-12-10 14:28:18 +0100

    client restructured

--- client.c
+++ client.c
@@ -6,44 +6,42 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/24 12:59:51 by alaparic          #+#    #+#             */
-/*   Updated: 2022/12/09 16:33:29 by alaparic         ###   ########.fr       */
+/*   Updated: 2022/12/10 14:25:59 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "client.h"
+#include <signal.h>
 #include "libft/libft.h"
 
-static void	send_binary(char c, int pid)
+static void	send_binary(int pid, char *str)
 {
 	int	i;
 
 	i = 0;
-	while (i < 8)
+	while (*str)
 	{
-		if (c & (1 << i++))
+		while (i < 8)
 		{
-			kill(pid, SIGUSR1);
-			printf("1");
+			if (*str & (1 << i++))
+			{
+				kill(pid, SIGUSR1);
+				ft_printf("1");
+			}
+			else
+			{
+				kill(pid, SIGUSR2);
+				ft_printf("0");
+			}
+			usleep(50);
 		}
-		else
-		{
-			kill(pid, SIGUSR2);
-			printf("0");
-		}
-		usleep(600);
+		str++;
 	}
 }
 
 int	main(int argc, char **argv)
 {
-	char	*message;
-	int		pid;
-
-	pid = ft_atoi(argv[1]);
-	message = argv[2];
-	while (*message)
-	{
-		send_binary(*message++, pid);
-	}
+	if (argc != 3)
+		return (1);
+	send_binary(ft_atoi(argv[1]), argv[2]);
 	return (0);
 }
